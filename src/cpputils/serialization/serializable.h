549542c5dd3e937c699438b040d1179ad4f4d6ba
KulahaRoman <kulaga.reo@gmail.com>
2022-10-14 19:30:57 +0300

    Modified serializable interface.

--- src/cpputils/serialization/serializable.h
+++ src/cpputils/serialization/serializable.h
@@ -2,15 +2,18 @@
 #include <functional>
 #include <memory>
 
+#include "../exceptions/exception.h"
+
 class InputStream;
 class OutputStream;
 
 class Serializable {
  public:
   virtual void Serialize(const std::shared_ptr<OutputStream> &stream) const = 0;
-  virtual void Serialize(
-      const std::shared_ptr<OutputStream> &stream,
-      const std::function<void(uint64_t)> &callback) const = 0;
+  virtual void Serialize(const std::shared_ptr<OutputStream> &stream,
+                         const std::function<void(uint64_t)> &onSuccess,
+                         const std::function<void(const Exception &)>
+                             &onFailure = nullptr) const = 0;
 
   virtual ~Serializable() {}
 };
