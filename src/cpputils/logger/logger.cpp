1b8a4601a7338984c5a04ced96aa3252d14ec706
Роман Кулага <romankulaha2010@gmail.com>
2022-12-14 21:14:59 +0200

    Reimplemented Logger with use of 'spdlog'.

--- src/cpputils/logger/logger.cpp
+++ src/cpputils/logger/logger.cpp
@@ -1,9 +1,10 @@
 #include "logger.h"
 
-Logger::Logger() { fout.open("logs.txt", std::ios::out | std::ios::app); }
-
-Logger::~Logger() {
-  if (fout.is_open()) {
-    fout.close();
-  }
+Logger::Logger()
+    : console(spdlog::stdout_color_mt(LOGGER_CONSOLE)),
+      file(spdlog::basic_logger_mt(LOGGER_FILE, LOGFILE)) {
+  spdlog::get(LOGGER_CONSOLE)
+      ->set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [TH %t] %v");
+  spdlog::get(LOGGER_FILE)
+      ->set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [TH %t] %v");
 }
--- src/cpputils/logger/logger.h
+++ src/cpputils/logger/logger.h
@@ -1,78 +1,52 @@
 #pragma once
-#include <fstream>
-#include <iostream>
-#include <mutex>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
 
-#ifdef __linux__
-enum class ConsoleColor {
-  RESET = "\\033[39m",
-  RED = "\\033[31m",
-  GREEN = "\\033[32m",
-  BLUE = "\\033[34m",
-  AQUA = "\\033[36m",
-  YELLOW = "\\033[33m",
-  PURPLE = "\\033[35m"
-};
-#elif _WIN32
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
+constexpr bool LOG_TO_FILE = true;
 
-enum class ConsoleColor : uint16_t {
-  RESET = 0x07,
-  RED = 0x04,
-  GREEN = 0x02,
-  BLUE = 0x01,
-  AQUA = 0x03,
-  YELLOW = 0x06,
-  PURPLE = 0x05
-};
-#endif
-
-#define LOGFILE
+constexpr char LOGFILE[] = "logs.txt";
+constexpr char LOGGER_CONSOLE[] = "console";
+constexpr char LOGGER_FILE[] = "file";
 
 class Logger {
  public:
   Logger();
-  ~Logger();
 
-  static void BeginBlock() { getInstance().mx.lock(); }
+  template <typename... Args>
+  static void Information(const std::string& fmt, Args&&... args) noexcept {
+    getInstance().console->info(fmt, std::forward<Args>(args)...);
 
-  static void EndBlock() { getInstance().mx.unlock(); }
-
-  template <typename Arg, typename... Args>
-  static void Information(Arg&& arg, Args&&... args) noexcept {
-    auto lock = std::unique_lock<std::recursive_mutex>(getInstance().mx);
-    setConsoleColor(ConsoleColor::AQUA);
-    getInstance().print("INFO", std::forward<Arg>(arg),
-                        std::forward<Args>(args)...);
-    setConsoleColor(ConsoleColor::RESET);
+    if (LOG_TO_FILE) {
+      getInstance().file->info(fmt, std::forward<Args>(args)...);
+    }
   }
 
-  template <typename Arg, typename... Args>
-  static void Warning(Arg&& arg, Args&&... args) noexcept {
-    auto lock = std::unique_lock<std::recursive_mutex>(getInstance().mx);
-    setConsoleColor(ConsoleColor::YELLOW);
-    getInstance().print("WARNING", std::forward<Arg>(arg),
-                        std::forward<Args>(args)...);
-    setConsoleColor(ConsoleColor::RESET);
+  template <typename... Args>
+  static void Warning(const std::string& fmt, Args&&... args) noexcept {
+    getInstance().console->warn(fmt, std::forward<Args>(args)...);
+
+    if (LOG_TO_FILE) {
+      getInstance().file->warn(fmt, std::forward<Args>(args)...);
+    }
   }
 
-  template <typename Arg, typename... Args>
-  static void Error(Arg&& arg, Args&&... args) noexcept {
-    auto lock = std::unique_lock<std::recursive_mutex>(getInstance().mx);
-    setConsoleColor(ConsoleColor::RED);
-    getInstance().print("ERROR", std::forward<Arg>(arg),
-                        std::forward<Args>(args)...);
-    setConsoleColor(ConsoleColor::RESET);
+  template <typename... Args>
+  static void Error(const std::string& fmt, Args&&... args) noexcept {
+    getInstance().console->error(fmt, std::forward<Args>(args)...);
+
+    if (LOG_TO_FILE) {
+      getInstance().file->error(fmt, std::forward<Args>(args)...);
+    }
   }
 
-  template <typename Arg, typename... Args>
-  static void Critical(Arg&& arg, Args&&... args) noexcept {
-    auto lock = std::unique_lock<std::recursive_mutex>(getInstance().mx);
-    setConsoleColor(ConsoleColor::PURPLE);
-    getInstance().print("CRITICAL", std::forward<Arg>(arg),
-                        std::forward<Args>(args)...);
-    setConsoleColor(ConsoleColor::RESET);
+  template <typename... Args>
+  static void Critical(const std::string& fmt, Args&&... args) noexcept {
+    getInstance().console->critical(fmt, std::forward<Args>(args)...);
+
+    if (LOG_TO_FILE) {
+      getInstance().file->critical(fmt, std::forward<Args>(args)...);
+    }
   }
 
  private:
@@ -81,33 +55,7 @@ class Logger {
     return logger;
   }
 
-  template <typename Arg, typename... Args>
-  void print(const std::string& keyWord, Arg&& arg, Args&&... args) noexcept {
-#ifdef LOGFILE
-    fout << "[THREAD " << std::this_thread::get_id() << "]";
-    fout << "[" << keyWord << "]: ";
-    fout << std::forward<Arg>(arg);
-    ((fout << std::forward<Args>(args)), ...);
-    fout << std::endl;
-#endif
-
-    std::cout << "[THREAD " << std::this_thread::get_id() << "]";
-    std::cout << "[" << keyWord << "]: ";
-    std::cout << std::forward<Arg>(arg);
-    ((std::cout << std::forward<Args>(args)), ...);
-    std::cout << std::endl;
-  }
-
-  static void setConsoleColor(ConsoleColor color) {
-#ifdef __linux__
-    std::cout << color;
-#elif _WIN32
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, static_cast<WORD>(color));
-#endif
-  }
-
  private:
-  std::ofstream fout;
-  std::recursive_mutex mx;
+  std::shared_ptr<spdlog::logger> console;
+  std::shared_ptr<spdlog::logger> file;
 };
