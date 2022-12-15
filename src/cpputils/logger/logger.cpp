32d4d7d1a37522ffba04cefbb06d78bcd4f1059a
Роман Кулага <romankulaha2010@gmail.com>
2022-12-15 17:38:04 +0200

    Changed logging pattern.

--- src/cpputils/logger/logger.cpp
+++ src/cpputils/logger/logger.cpp
@@ -4,7 +4,7 @@ Logger::Logger()
     : console(spdlog::stdout_color_mt(LOGGER_CONSOLE)),
       file(spdlog::basic_logger_mt(LOGGER_FILE, LOGFILE)) {
   spdlog::get(LOGGER_CONSOLE)
-      ->set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [TH %t] %v");
+      ->set_pattern("[%H:%M:%S %z][TH %t][%^---%L---%$]: %v");
   spdlog::get(LOGGER_FILE)
-      ->set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [TH %t] %v");
+      ->set_pattern("[%H:%M:%S %z][TH %t][%^---%L---%$]: %v");
 }
