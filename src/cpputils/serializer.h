34f7e7b13a641267fa07fb912250e6397fd47049
Roman Kulaha <romankulaha2010@gmail.com>
2026-01-23 12:54:41 +0100

    moved back to separate serializer.cpp

--- src/cpputils/serializer.h
+++ src/cpputils/serializer.h
@@ -948,16 +948,5 @@ class Serializer {
   static thread_local std::map<std::shared_ptr<void>, int> referenceMap;
   static thread_local std::map<int, std::shared_ptr<void>> reverseReferenceMap;
 };
-
-const int Serializer::UNDEFINED_REFERENCE_INDEX = -1;
-const int Serializer::UNDEFINED_OBJECT_UNIQUE_ID = -1;
-
-thread_local int Serializer::referenceCount = 0;
-
-thread_local long long Serializer::initialStackFrameMarker = 0;
-
-thread_local std::map<std::shared_ptr<void>, int> Serializer::referenceMap;
-thread_local std::map<int, std::shared_ptr<void>>
-    Serializer::reverseReferenceMap;
 }  // namespace Serialization
 }  // namespace CppUtils
